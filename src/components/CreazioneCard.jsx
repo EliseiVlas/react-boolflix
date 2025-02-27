270005787de9b8f642064c244d296a3dbedf49e6
Elisei <eliseivlas.002@gmail.com>
2025-02-27 17:40:39 +0100

    AGGIUNTA BANDIERE E consegna 2 milestone

--- src/components/CreazioneCard.jsx
+++ src/components/CreazioneCard.jsx
@@ -1,7 +1,28 @@
+import Flag from 'react-world-flags'
+
+const languageToCountry = {
+    en: "GB", 
+    it: "IT", 
+    fr: "FR", 
+    de: "DE", 
+    es: "ES", 
+    ja: "JP", 
+    ko: "KR", 
+    zh: "CN", 
+    ru: "RU", 
+    pt: "PT", 
+    nl: "NL",
+    pl: "PL",
+    da: "DA",
+    fa: "FA"
+};
 const CreazioneCard = (props) => {
 
     // destructuring dell'oggetto props
     const { film } = props;
+
+     // Trova il codice paese corrispondente alla lingua
+     const countryCode = languageToCountry[film.original_language];
 
     return (
         <>
@@ -9,11 +30,14 @@ const CreazioneCard = (props) => {
             <img src={`https://image.tmdb.org/t/p/w342/${film.poster_path}`} alt={film.title || film.name} />
                 <h1>{film.title || film.name}</h1>
                 <h2>{film.original_title || film.original_name}</h2>
-                <span>{film.original_language}</span>
+                {countryCode ? (
+                    <Flag code={countryCode} style={{ width: "30px", height: "20px" }} />
+                ) : (
+                    <span>{film.original_language}</span> // Se non trova il codice, mostra un'icona globale
+                )}
                 <br />
                 <span>{film.vote_average}</span>
             </div>
-
         </>
     )
 
